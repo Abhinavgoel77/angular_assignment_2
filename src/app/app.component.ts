f078acdd482098537888c93c4e71c1cc2962b533
Abhinav goel <Abhinavgoel77@gmail.com>
2021-08-16 18:39:59 +0530

    added screenshots

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'form';
+
+  userModel = new User('','','',9999999999,"dd/mm/yyyy ");
+
+  onSubmit(){
+     console.log(this.userModel);
+  }
 }
